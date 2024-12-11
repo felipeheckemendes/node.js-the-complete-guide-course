b66256dea5b8833874067a5e0fa94adf5a898b65
felipeheckemendes <157503990+felipeheckemendes@users.noreply.github.com>
2024-12-11 20:32:23 -0300

    feat: add virtual property (durationWeebs)

--- project1-natours-API/models/tourModel.js
+++ project1-natours-API/models/tourModel.js
@@ -1,58 +1,68 @@
 const mongoose = require('mongoose');
 
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'Tour name required'],
-    unique: true,
+const tourSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Tour name required'],
+      unique: true,
+    },
+    duration: {
+      type: Number,
+      required: [true, 'Tour duration required'],
+    },
+    maxGroupSize: {
+      type: Number,
+      required: [true, 'Tour max group size required'],
+    },
+    difficulty: {
+      type: String,
+      required: [true, 'Tour dificulty required'],
+    },
+    ratingsAverage: {
+      type: Number,
+      default: 4.5,
+    },
+    ratingsQuantity: {
+      type: Number,
+      default: 0,
+    },
+    price: {
+      type: Number,
+      required: [true, 'Tour price required'],
+    },
+    discount: {
+      type: Number,
+    },
+    summary: {
+      type: String,
+      trim: true,
+      required: [true, 'Tour summary required'],
+    },
+    description: {
+      type: String,
+      trim: true,
+    },
+    imageCover: {
+      type: String,
+      required: [true, 'Tour image required'],
+    },
+    images: [String],
+    startDates: [Date],
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+      select: false,
+    },
   },
-  duration: {
-    type: Number,
-    required: [true, 'Tour duration required'],
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
   },
-  maxGroupSize: {
-    type: Number,
-    required: [true, 'Tour max group size required'],
-  },
-  difficulty: {
-    type: String,
-    required: [true, 'Tour dificulty required'],
-  },
-  ratingsAverage: {
-    type: Number,
-    default: 4.5,
-  },
-  ratingsQuantity: {
-    type: Number,
-    default: 0,
-  },
-  price: {
-    type: Number,
-    required: [true, 'Tour price required'],
-  },
-  discount: {
-    type: Number,
-  },
-  summary: {
-    type: String,
-    trim: true,
-    required: [true, 'Tour summary required'],
-  },
-  description: {
-    type: String,
-    trim: true,
-  },
-  imageCover: {
-    type: String,
-    required: [true, 'Tour image required'],
-  },
-  images: [String],
-  startDates: [Date],
-  createdAt: {
-    type: Date,
-    default: Date.now(),
-    select: false,
-  },
+);
+
+tourSchema.virtual('durationWeeks').get(function () {
+  return this.duration / 7;
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
