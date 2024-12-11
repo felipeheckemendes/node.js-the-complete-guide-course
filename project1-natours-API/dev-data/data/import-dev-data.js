a6e1babdc86dd7eba7208fe423bbad8a91be559b
felipeheckemendes <157503990+felipeheckemendes@users.noreply.github.com>
2024-12-11 06:17:08 -0300

    feat: add more attributes to the tours model/schema

---
+++ project1-natours-API/dev-data/data/import-dev-data.js
@@ -0,0 +1,52 @@
+const fs = require('fs');
+const path = require('path');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const readline = require('readline');
+const Tour = require('../../models/tourModel');
+
+dotenv.config({ path: path.join(__dirname, '..', '..', 'config.env') });
+// const app = require('../../app'); // The app should be required AFTER configuring the env variables, because requiring runs the file
+
+// Connection to Mongoose
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then((con) => {
+    console.log('\nMongoDB Database Connection Succesfull');
+  });
+
+// Read JSON file
+const fileData = fs.readFileSync(path.join(__dirname, 'tours-simple.json'), 'utf-8');
+
+// Function to import data into DB
+const importData = async () => {
+  try {
+    await Tour.create(JSON.parse(fileData));
+    console.log('✅ Data Successfully loaded');
+  } catch (err) {
+    console.log('🧨 ERROR', err.message);
+  }
+  process.exit();
+};
+
+// Function to delete all data from DB collection
+const deleteData = async () => {
+  try {
+    await Tour.deleteMany();
+    console.log('⭕ Data succesfully deleted');
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+
+if (process.argv[2] === '--import') {
+  importData();
+} else if (process.argv[2] === '--delete') {
+  deleteData();
+}
--- project1-natours-API/models/tourModel.js
+++ project1-natours-API/models/tourModel.js
@@ -6,14 +6,52 @@ const tourSchema = new mongoose.Schema({
     required: [true, 'Tour name required'],
     unique: true,
   },
-  rating: {
+  duration: {
+    type: Number,
+    required: [true, 'Tour duration required'],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'Tour max group size required'],
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'Tour dificulty required'],
+  },
+  ratingsAverage: {
     type: Number,
     default: 4.5,
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0,
   },
   price: {
     type: Number,
     required: [true, 'Tour price required'],
   },
+  discount: {
+    type: Number,
+  },
+  summary: {
+    type: String,
+    trim: true,
+    required: [true, 'Tour summary required'],
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'Tour image required'],
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  startDates: [Date],
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
