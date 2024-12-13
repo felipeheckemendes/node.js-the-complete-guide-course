36425803852f2561620faf0d645717e4f7562960
felipeheckemendes <157503990+felipeheckemendes@users.noreply.github.com>
2024-12-13 18:48:28 -0300

    feat: add distinct error responses for production and development environments; handle database errors: invalid ID, duplicate fields, field validation errors

--- project1-natours-API/controllers/errorController.js
+++ project1-natours-API/controllers/errorController.js
@@ -1,8 +1,67 @@
-module.exports = (err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'error';
+const AppError = require('../utils/appError');
+
+const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
     message: err.message,
+    error: err,
+    stack: err.stack,
   });
+};
+
+const sendErrorProd = (err, res) => {
+  if (err.isOperational) {
+    // Operation error (trusted)
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  } else {
+    // Programming or another unknow error: don't leak error details
+    console.error('ERROR 🧨', err); // Log the error
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went wrong.',
+    });
+  }
+};
+
+const handleCastErrorDB = (err) => {
+  const message = `Invalid ${err.path}: ${err.value}`;
+  return new AppError(message, err.statusCode);
+};
+
+const handleDuplicateFieldsDB = (err) => {
+  const message = `Duplicate field value: ${err.errmsg.match(/(?<=(["']\b))(?:(?=(\\?))\2.)*?(?=\1)/)}. Please use another value.`;
+  return new AppError(message, err.statusCode);
+};
+
+const handleValidationErrorDB = (err) => {
+  errors = Object.values(err.errors).map((el) => el.message);
+  const message = `Invalid input data. ${errors.join('. ')}`;
+  return new AppError(message, err.statusCode);
+};
+
+module.exports = (err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    console.log(err);
+    let error = err;
+    if (err.name === 'CastError') {
+      // Handle cast error (mongoose) to display meaningful message
+      error = handleCastErrorDB(err);
+    }
+    if (err.code === 11000) {
+      // Handle duplicate values error (11000 Mongo error code)
+      error = handleDuplicateFieldsDB(err);
+    }
+    if (err.name === 'ValidationError') {
+      // Handle validation error (mongoose)
+      error = handleValidationErrorDB(err);
+    }
+    sendErrorProd(error, res);
+  }
 };
